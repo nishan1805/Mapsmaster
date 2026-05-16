f8bb153c0282ec03c1a4dd50cad2f7c17dd0a650
nishan1805 <76156501+nishan1805@users.noreply.github.com>
2026-05-16 15:59:08 +0530

    Update WhatsAppButton.tsx

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion } from "framer-motion";';
+import { motion } from "framer-motion";
 
 export default function WhatsAppButton() {
   const phoneNumber = '9179775502';
