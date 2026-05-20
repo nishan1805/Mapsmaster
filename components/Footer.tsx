84f6d0de51b4531f0eff7caba605a18b4c680a99
nishan1805 <76156501+nishan1805@users.noreply.github.com>
2026-05-21 00:22:14 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,14 +21,14 @@ const Footer: React.FC = () => {
           </p>
 
           <div className="flex gap-6">
-            <a href="https://instagram.com/mapsmasterraipur" target="_blank" rel="noopener noreferrer" className="w-12 h-12 rounded-full bg-white/5 flex items-center justify-center text-white hover:bg-googleBlue transition-all">
+            <a href="https://www.instagram.com/mapsmaster_raipur/" target="_blank" rel="noopener noreferrer" className="w-12 h-12 rounded-full bg-white/5 flex items-center justify-center text-white hover:bg-googleBlue transition-all">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-instagram"><rect width="20" height="20" x="2" y="2" rx="5" ry="5"/><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/><line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/></svg>
             </a>
-            <a href="https://facebook.com/mapsmasterraipur" target="_blank" rel="noopener noreferrer" className="w-12 h-12 rounded-full bg-white/5 flex items-center justify-center text-white hover:bg-googleBlue transition-all">
+            <a href="https://www.facebook.com/profile.php?id=61585099034811" target="_blank" rel="noopener noreferrer" className="w-12 h-12 rounded-full bg-white/5 flex items-center justify-center text-white hover:bg-googleBlue transition-all">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-facebook"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/></svg>
             </a>
             <a
-              href="https://linkedin.com/company/mapsmaster"
+              href="https://www.linkedin.com/company/maps-master/"
               target="_blank"
               rel="noopener noreferrer"
               className="w-12 h-12 rounded-full bg-white/5 flex items-center justify-center text-white hover:bg-googleBlue transition-all"
