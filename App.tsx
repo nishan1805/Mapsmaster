bb2647ac0889f858af3fcd4add46238818cb76be
nishan1805 <76156501+nishan1805@users.noreply.github.com>
2026-06-22 06:08:01 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,31 +1,31 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import TrustStrip from './components/TrustStrip';
 import Services from './components/Services';
 import Clients from './components/Clients';
-import Packages from './components/Packages';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import WhatsAppButton from './components/WhatsAppButton';
 
 const App: React.FC = () => {
+  const [selectedService, setSelectedService] = useState<string>('');
+  const [isCompareOpen, setIsCompareOpen] = useState<boolean>(false);
+
   return (
     <div className="min-h-screen bg-white">
       <Header />
       <main>
         <Hero />
         <TrustStrip />
-        <Services />
+        <Services onSelectPackage={setSelectedService} onCompareModalToggle={setIsCompareOpen} />
         <Clients />
-        <Packages />
         <Testimonials />
-        <Contact />
+        <Contact selectedService={selectedService} onSelectedServiceChange={setSelectedService} />
       </main>
       <Footer />
-      <WhatsAppButton />
+      <WhatsAppButton isCompareOpen={isCompareOpen} />
     </div>
   );
 };
